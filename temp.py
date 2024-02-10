f4fec04fa4273341df7ce8fbbef5774aa836cf18
Sreeraj MP Nair <134491125+sree-raj-nair@users.noreply.github.com>
2024-02-10 12:38:04 +0530

    Updated  temp.py

--- temp.py
+++ temp.py
@@ -1,2 +1,7 @@
-print("Welcome to Github tutorial")
-print("Changes made in this file to check")
+import random
+my_list=[2,45,34,57,8888]
+print(random.randint(1,12))
+print(random.random())
+print(random.choice([1,2,45,69]))
+random.shuffle(my_list)
+print(my_list)
